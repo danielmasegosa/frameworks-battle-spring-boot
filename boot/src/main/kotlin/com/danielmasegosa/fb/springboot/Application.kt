ea010f6496b48de5ee1ba05c77889a98694bf2a1
Daniel Masegosa <danielmasegosa@hotmail.com>
2020-03-22 19:55:34 +0100

    [SETUP] Initial setup. Clean Architecture structure added

--- boot/src/main/kotlin/com/danielmasegosa/fb/springboot/Application.kt
+++ boot/src/main/kotlin/com/danielmasegosa/fb/springboot/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class FrameworksBattleSpringBootApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<FrameworksBattleSpringBootApplication>(*args)
+    runApplication<Application>(*args)
 }
